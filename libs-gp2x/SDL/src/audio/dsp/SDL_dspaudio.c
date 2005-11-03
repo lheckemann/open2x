9e8f77c43889ca3fce10efc0ab6748c86cd47bd3
djwillis <djwillis@3edcfc3d-570e-0410-a036-8bc182068dc2>
2005-11-03 21:41:23 +0000

    Initial check-in of GPH patched SDL (1.2.7) as a basis for cleaning up as part of the wider GP2X/Linux SDL optimisation work. This is a rough and ready check-in.
    git-svn-id: svn://svn.code.sf.net/p/open2x/code/trunk@22 3edcfc3d-570e-0410-a036-8bc182068dc2

---
+++ libs-gp2x/SDL/src/audio/dsp/SDL_dspaudio.c
@@ -0,0 +1,518 @@
+/*
+    SDL - Simple DirectMedia Layer
+    Copyright (C) 1997-2004 Sam Lantinga
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    Sam Lantinga
+    slouken@libsdl.org
+*/
+
+#ifdef SAVE_RCSID
+static char rcsid =
+ "@(#) $Id$";
+#endif
+
+/* Allow access to a raw mixing buffer */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/time.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#ifdef OSS_USE_SOUNDCARD_H
+/* This is installed on some systems */
+#include <soundcard.h>
+#else
+/* This is recommended by OSS */
+#include <sys/soundcard.h>
+#endif
+
+#include "SDL_audio.h"
+#include "SDL_error.h"
+#include "SDL_audiomem.h"
+#include "SDL_audio_c.h"
+#include "SDL_timer.h"
+#include "SDL_audiodev_c.h"
+#include "SDL_dspaudio.h"
+
+//#define DEBUG_AUDIO
+//#define GHCDEBUG // ghcstop
+#ifdef GHCDEBUG
+#define ghcd(x) printf("(%s, %d), %s: ", __FILE__, __LINE__, __FUNCTION__); printf x;
+#else
+#define ghcd(x)
+#endif
+
+
+/* The tag name used by DSP audio */
+#define DSP_DRIVER_NAME         "dsp"
+
+#if 1 // ghcstop
+/* Open the audio device for playback, and don't block if busy */
+/*#define USE_BLOCKING_WRITES*/
+#define OPEN_FLAGS	(O_WRONLY|O_NONBLOCK)
+#else
+/* Open the audio device for playback, and don't block if busy */
+#define USE_BLOCKING_WRITES // ghcstop fix, swl-patch non-blocking error?
+#define OPEN_FLAGS	(O_WRONLY)
+#endif
+
+
+
+#if 0
+typedef struct {
+	int freq;		/* DSP frequency -- samples per second */
+	Uint16 format;		/* Audio data format */
+	Uint8  channels;	/* Number of channels: 1 mono, 2 stereo */
+	Uint8  silence;		/* Audio buffer silence value (calculated) */
+	Uint16 samples;		/* Audio buffer size in samples (power of 2) */
+	Uint16 padding;		/* Necessary for some compile environments */
+	Uint32 size;		/* Audio buffer size in bytes (calculated) */
+	/* This function is called when the audio device needs more data.
+	   'stream' is a pointer to the audio data buffer
+	   'len' is the length of that buffer in bytes.
+	   Once the callback returns, the buffer will no longer be valid.
+	   Stereo samples are stored in a LRLRLR ordering.
+	*/
+	void (*callback)(void *userdata, Uint8 *stream, int len);
+	void  *userdata;
+} SDL_AudioSpec;
+#endif
+
+#ifdef DEBUG_AUDIO
+static void Print_AudioSpec(SDL_AudioSpec *spec)
+{
+	ghcd(("\nfreq     = %d\n", spec->freq));
+	ghcd(("format   = 0x%4.4x\n", spec->format));
+	ghcd(("channels = %d\n", spec->channels));
+	ghcd(("silence  = %x\n", spec->silence));
+	ghcd(("samples  = %d\n", spec->samples));
+	ghcd(("padding  = %d\n", spec->padding));
+	ghcd(("size     = %d\n\n", spec->size));
+}
+#endif
+
+
+/* Audio driver functions */
+static int DSP_OpenAudio(_THIS, SDL_AudioSpec *spec);
+static void DSP_WaitAudio(_THIS);
+static void DSP_PlayAudio(_THIS);
+static Uint8 *DSP_GetAudioBuf(_THIS);
+static void DSP_CloseAudio(_THIS);
+
+/* Audio driver bootstrap functions */
+
+static int Audio_Available(void)
+{
+	int fd;
+	int available;
+
+	available = 0;
+	fd = SDL_OpenAudioPath(NULL, 0, OPEN_FLAGS, 0);
+	if ( fd >= 0 ) {
+		available = 1;
+		close(fd);
+	}
+	return(available);
+}
+
+static void Audio_DeleteDevice(SDL_AudioDevice *device)
+{
+	free(device->hidden);
+	free(device);
+}
+
+static SDL_AudioDevice *Audio_CreateDevice(int devindex)
+{
+	SDL_AudioDevice *this;
+
+	/* Initialize all variables that we clean on shutdown */
+	this = (SDL_AudioDevice *)malloc(sizeof(SDL_AudioDevice));
+	if ( this ) {
+		memset(this, 0, (sizeof *this));
+		this->hidden = (struct SDL_PrivateAudioData *)
+				malloc((sizeof *this->hidden));
+	}
+	if ( (this == NULL) || (this->hidden == NULL) ) {
+		SDL_OutOfMemory();
+		if ( this ) {
+			free(this);
+		}
+		return(0);
+	}
+	memset(this->hidden, 0, (sizeof *this->hidden));
+	audio_fd = -1;
+
+	/* Set the function pointers */
+	this->OpenAudio = DSP_OpenAudio;
+	this->WaitAudio = DSP_WaitAudio;
+	this->PlayAudio = DSP_PlayAudio;
+	this->GetAudioBuf = DSP_GetAudioBuf;
+	this->CloseAudio = DSP_CloseAudio;
+
+	this->free = Audio_DeleteDevice;
+
+	return this;
+}
+
+AudioBootStrap DSP_bootstrap = {
+	DSP_DRIVER_NAME, "OSS /dev/dsp standard audio",
+	Audio_Available, Audio_CreateDevice
+};
+
+/* This function waits until it is possible to write a full sound buffer */
+static void DSP_WaitAudio(_THIS)
+{
+	/* Check to see if the thread-parent process is still alive */
+	{ static int cnt = 0;
+		/* Note that this only works with thread implementations 
+		   that use a different process id for each thread.
+		*/
+		if (parent && (((++cnt)%10) == 0)) { /* Check every 10 loops */
+			if ( kill(parent, 0) < 0 ) {
+				this->enabled = 0;
+			}
+		}
+	}
+
+#ifndef USE_BLOCKING_WRITES /* Not necessary when using blocking writes */
+	/* See if we need to use timed audio synchronization */
+	if ( frame_ticks ) {
+		/* Use timer for general audio synchronization */
+		Sint32 ticks;
+
+		ticks = ((Sint32)(next_frame - SDL_GetTicks()))-FUDGE_TICKS;
+		if ( ticks > 0 ) {
+			SDL_Delay(ticks);
+		}
+	} else {
+		/* Use select() for audio synchronization */
+		fd_set fdset;
+		struct timeval timeout;
+
+		FD_ZERO(&fdset);
+		FD_SET(audio_fd, &fdset);
+		timeout.tv_sec = 10;
+		timeout.tv_usec = 0;
+
+		ghcd(( "Waiting for audio to get ready\n"));
+
+		if ( select(audio_fd+1, NULL, &fdset, NULL, &timeout) <= 0 ) {
+			const char *message =
+			"Audio timeout - buggy audio driver? (disabled)";
+			/* In general we should never print to the screen,
+			   but in this case we have no other way of letting
+			   the user know what happened.
+			*/
+			fprintf(stderr, "SDL: %s\n", message);
+			this->enabled = 0;
+			/* Don't try to close - may hang */
+			audio_fd = -1;
+
+			ghcd(( "Done disabling audio\n"));
+		}
+		ghcd(( "Ready!\n"));
+	}
+#endif /* !USE_BLOCKING_WRITES */
+}
+
+static void DSP_PlayAudio(_THIS)
+{
+	int written, p=0;
+
+	/* Write the audio data, checking for EAGAIN on broken audio drivers */
+	do {
+		written = write(audio_fd, &mixbuf[p], mixlen-p);
+		if (written>0)
+		   p += written;
+		if (written == -1 && errno != 0 && errno != EAGAIN && errno != EINTR)
+		{
+                   ghcd(("bad file descriptor\n"));
+		   /* Non recoverable error has occurred. It should be reported!!! */
+		   perror("audio");
+		   break;
+		}
+
+		if ( p < written || ((written < 0) && ((errno == 0) || (errno == EAGAIN))) ) {
+			SDL_Delay(1);	/* Let a little CPU time go by */
+		}
+	} while ( p < written );
+
+	/* If timer synchronization is enabled, set the next write frame */
+	if ( frame_ticks ) {
+		next_frame += frame_ticks;
+	}
+
+	/* If we couldn't write, assume fatal error for now */
+	if ( written < 0 ) {
+		this->enabled = 0;
+	}
+
+	ghcd(( "Wrote %d bytes of audio data\n", written));
+}
+
+static Uint8 *DSP_GetAudioBuf(_THIS)
+{
+	return(mixbuf);
+}
+
+static void DSP_CloseAudio(_THIS)
+{
+	if ( mixbuf != NULL ) {
+		SDL_FreeAudioMem(mixbuf);
+		mixbuf = NULL;
+	}
+	if ( audio_fd >= 0 ) {
+		int value;
+		ioctl(audio_fd, SNDCTL_DSP_RESET, &value);
+		close(audio_fd);
+		audio_fd = -1;
+	}
+}
+
+static int DSP_ReopenAudio(_THIS, const char *audiodev, int format,
+						SDL_AudioSpec *spec)
+{
+	int frag_spec;
+	int value;
+
+	/* Close and then reopen the audio device */
+	close(audio_fd);
+	audio_fd = open(audiodev, O_WRONLY, 0);
+	if ( audio_fd < 0 ) {
+		SDL_SetError("Couldn't open %s: %s", audiodev, strerror(errno));
+		return(-1);
+	}
+
+	/* Calculate the final parameters for this audio specification */
+	SDL_CalculateAudioSpec(spec);
+
+	/* Determine the power of two of the fragment size */
+	for ( frag_spec = 0; (0x01<<frag_spec) < spec->size; ++frag_spec );
+	if ( (0x01<<frag_spec) != spec->size ) {
+		SDL_SetError("Fragment size must be a power of two");
+		return(-1);
+	}
+	//frag_spec |= 0x00020000;	/* two fragments, for low latency */
+	frag_spec |= 0x00100000;	/* ghcsto, 16 fragments, 2 fragment를 쓰면 소리가 띡띡 끊긴다 */
+
+	/* Set the audio buffering parameters */
+	ghcd(( "Requesting %d fragments of size %d\n",(frag_spec >> 16), 1<<(frag_spec&0xFFFF) ));
+
+	if ( ioctl(audio_fd, SNDCTL_DSP_SETFRAGMENT, &frag_spec) < 0 ) {
+		fprintf(stderr, "Warning: Couldn't set audio fragment size\n");
+	}
+#ifdef DEBUG_AUDIO
+	{ audio_buf_info info;
+	  ioctl(audio_fd, SNDCTL_DSP_GETOSPACE, &info);
+	  ghcd(( "fragments = %d\n", info.fragments));
+	  ghcd(( "fragstotal = %d\n", info.fragstotal));
+	  ghcd(( "fragsize = %d\n", info.fragsize));
+	  ghcd(( "bytes = %d\n", info.bytes));
+	}
+#endif
+
+	/* Set the audio format */
+	value = format;
+	if ( (ioctl(audio_fd, SNDCTL_DSP_SETFMT, &value) < 0) ||
+						(value != format) ) {
+		SDL_SetError("Couldn't set audio format");
+		return(-1);
+	}
+
+	/* Set the number of channels of output */
+	value = spec->channels;
+#ifdef SNDCTL_DSP_CHANNELS
+	if ( ioctl(audio_fd, SNDCTL_DSP_CHANNELS, &value) < 0 ) {
+#endif
+		value = (spec->channels > 1);
+		ioctl(audio_fd, SNDCTL_DSP_STEREO, &value);
+		value = (value ? 2 : 1);
+#ifdef SNDCTL_DSP_CHANNELS
+	}
+#endif
+	if ( value != spec->channels ) {
+		SDL_SetError("Couldn't set audio channels");
+		return(-1);
+	}
+
+	/* Set the DSP frequency */
+	value = spec->freq;
+
+        // ghcstop: s3c2410에서는 이 부분이 구현안되어 있고, SNDCTL_DSP_SPEED으로 구현되어 있다.
+        #if 0 // ghcstop
+	if ( ioctl(audio_fd, SOUND_PCM_WRITE_RATE, &value) < 0 ) {
+        #endif
+       	if ( ioctl(audio_fd, SNDCTL_DSP_SPEED, &value) < 0 )
+        {
+		SDL_SetError("Couldn't set audio frequency");
+		return(-1);
+	}
+	spec->freq = value;
+
+	/* We successfully re-opened the audio */
+	return(0);
+}
+
+static int DSP_OpenAudio(_THIS, SDL_AudioSpec *spec)
+{
+	char audiodev[1024];
+	int format;
+	int value;
+	Uint16 test_format;
+
+	/* Reset the timer synchronization flag */
+	frame_ticks = 0.0;
+
+	/* Open the audio device */
+	audio_fd = SDL_OpenAudioPath(audiodev, sizeof(audiodev), OPEN_FLAGS, 0);
+	if ( audio_fd < 0 ) {
+		SDL_SetError("Couldn't open %s: %s", audiodev, strerror(errno));
+		return(-1);
+	}
+	mixbuf = NULL;
+
+#ifdef USE_BLOCKING_WRITES
+	/* Make the file descriptor use blocking writes with fcntl() */
+	{ long flags;
+		flags = fcntl(audio_fd, F_GETFL);
+		flags &= ~O_NONBLOCK;
+		if ( fcntl(audio_fd, F_SETFL, flags) < 0 ) {
+			SDL_SetError("Couldn't set audio blocking mode");
+			return(-1);
+		}
+	}
+#endif
+
+	/* Get a list of supported hardware formats */
+	if ( ioctl(audio_fd, SNDCTL_DSP_GETFMTS, &value) < 0 ) {
+		SDL_SetError("Couldn't get audio format list");
+		return(-1);
+	}
+
+	/* Try for a closest match on audio format */
+	format = 0;
+	for ( test_format = SDL_FirstAudioFormat(spec->format);
+						! format && test_format; ) {
+#ifdef DEBUG_AUDIO
+		ghcd(( "Trying format 0x%4.4x\n", test_format)) ;
+#endif
+		switch ( test_format ) {
+			case AUDIO_U8:
+				if ( value & AFMT_U8 ) {
+					format = AFMT_U8;
+				}
+				break;
+			case AUDIO_S8:
+				if ( value & AFMT_S8 ) {
+					format = AFMT_S8;
+				}
+				break;
+			case AUDIO_S16LSB:
+				if ( value & AFMT_S16_LE ) {
+					format = AFMT_S16_LE;
+				}
+				break;
+			case AUDIO_S16MSB:
+				if ( value & AFMT_S16_BE ) {
+					format = AFMT_S16_BE;
+				}
+				break;
+			case AUDIO_U16LSB:
+				if ( value & AFMT_U16_LE ) {
+					format = AFMT_U16_LE;
+				}
+				break;
+			case AUDIO_U16MSB:
+				if ( value & AFMT_U16_BE ) {
+					format = AFMT_U16_BE;
+				}
+				break;
+			default:
+				format = 0;
+				break;
+		}
+		if ( ! format ) {
+			test_format = SDL_NextAudioFormat();
+		}
+	}
+	if ( format == 0 ) {
+		SDL_SetError("Couldn't find any hardware audio formats");
+		return(-1);
+	}
+	spec->format = test_format;
+	ghcd(( "Tested format 0x%4.4x\n", spec->format));
+
+	/* Set the audio format */
+	value = format;
+	if ( (ioctl(audio_fd, SNDCTL_DSP_SETFMT, &value) < 0) ||
+						(value != format) ) {
+		SDL_SetError("Couldn't set audio format");
+		return(-1);
+	}
+
+	/* Set the number of channels of output */
+	value = spec->channels;
+#ifdef SNDCTL_DSP_CHANNELS
+	if ( ioctl(audio_fd, SNDCTL_DSP_CHANNELS, &value) < 0 ) {
+#endif
+		value = (spec->channels > 1);
+		ioctl(audio_fd, SNDCTL_DSP_STEREO, &value);
+		value = (value ? 2 : 1);
+#ifdef SNDCTL_DSP_CHANNELS
+	}
+#endif
+	spec->channels = value;
+
+	/* Because some drivers don't allow setting the buffer size
+	   after setting the format, we must re-open the audio device
+	   once we know what format and channels are supported
+	 */
+	if ( DSP_ReopenAudio(this, audiodev, format, spec) < 0 ) {
+		/* Error is set by DSP_ReopenAudio() */
+		return(-1);
+	}
+
+	/* Allocate mixing buffer */
+	mixlen = spec->size;
+	mixbuf = (Uint8 *)SDL_AllocAudioMem(mixlen);
+	if ( mixbuf == NULL ) {
+		return(-1);
+	}
+	memset(mixbuf, spec->silence, spec->size);
+
+#ifndef USE_BLOCKING_WRITES
+	/* Check to see if we need to use select() workaround */
+	{ char *workaround;
+		workaround = getenv("SDL_DSP_NOSELECT");
+		if ( workaround ) {
+			frame_ticks = (float)(spec->samples*1000)/spec->freq;
+			next_frame = SDL_GetTicks()+frame_ticks;
+		}
+	}
+#endif /* !USE_BLOCKING_WRITES */
+
+	/* Get the parent process id (we're the parent of the audio thread) */
+	parent = getpid();
+
+	/* We're ready to rock and roll. :-) */
+	return(0);
+}
