2ad079d64c6beabaf1fc03f24ab55c40b28745f7
lithosphere <lithosphere@3edcfc3d-570e-0410-a036-8bc182068dc2>
2008-07-29 18:34:38 +0000

    Latest version of libmpeg2 with ARM optimizations
    git-svn-id: svn://svn.code.sf.net/p/open2x/code/trunk@310 3edcfc3d-570e-0410-a036-8bc182068dc2

--- libs-new/mpeg2dec/gp2x.mk
+++ libs-new/mpeg2dec/gp2x.mk
@@ -1,7 +1,15 @@
-MPEG2DEC_DIR := mpeg2dec/mpeg2dec-gp2x
+MPEG2DEC_DIR_ROOT := mpeg2dec
+MPEG2DEC_DIR := mpeg2dec/libmpeg2-0.5.1
 
-o2x-mpeg2dec: o2x-sdl
+o2x-mpeg2dec: #o2x-sdl
 	@-echo "Build mpeg2dec"
+
+	@-(cd $(MPEG2DEC_DIR_ROOT); \
+	echo " extracting mpeg2dec"; \
+	tar -zxf libmpeg2-0.5.1.tar.gz; \
+	echo " commenting pld instructions"; \
+	sed s/pld/@pld/ libmpeg2-0.5.1/libmpeg2/motion_comp_arm_s.S > libmpeg2-0.5.1/libmpeg2/motion_comp_arm_s.S.new && mv libmpeg2-0.5.1/libmpeg2/motion_comp_arm_s.S.new libmpeg2-0.5.1/libmpeg2/motion_comp_arm_s.S )
+
 	(cd $(MPEG2DEC_DIR); rm -rf config.cache; \
 		$(FLAGS) ./configure --prefix=$(PREFIX)\
 				--target=$(TARGET)\
@@ -9,6 +17,7 @@ o2x-mpeg2dec: o2x-sdl
 				--build=$(BUILD)\
 				--enable-shared\
 				--enable-static\
+				--disable-sdl\
 				--with-sdl-prefix=$(PREFIX)\
 	)
 	$(FLAGS) make -C $(MPEG2DEC_DIR)
